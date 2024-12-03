abb72e4d720b4c5126e32b6f8a08f96391ffbaaf
kasey brown <laseyrow@gmail.com>
2024-12-03 13:40:33 -0700

    start wpf

---
+++ wpf decks/wpf decks/MainWindow.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        purdue.Visibility = Visibility.Visible
+        georgia.Visibility = Visibility.Hidden
+        ohio.Visibility = Visibility.Hidden
+        founder.Content = "It was founded on May 6, 1869"
+    End Sub
+End Class
