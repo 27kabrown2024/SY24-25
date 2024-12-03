ccb02f1cae0f7e4203d734375131e689f7ca5c1d
kasey brown <laseyrow@gmail.com>
2024-12-03 13:20:37 -0700

    idk

--- tetris/tetris/Form1.Designer.vb
+++ tetris/tetris/Form1.Designer.vb
@@ -30,6 +30,8 @@ Partial Class Form1
         enemy2 = New PictureBox()
         enemy3 = New PictureBox()
         platform = New PictureBox()
+        scorelabel = New Label()
+        Timer2 = New Timer(components)
         CType(avatar, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
         CType(enemy, ComponentModel.ISupportInitialize).BeginInit()
@@ -41,9 +43,9 @@ Partial Class Form1
         ' avatar
         ' 
         avatar.Image = My.Resources.Resources.pdiddy
-        avatar.Location = New Point(384, 77)
+        avatar.Location = New Point(432, 69)
         avatar.Name = "avatar"
-        avatar.Size = New Size(35, 53)
+        avatar.Size = New Size(39, 48)
         avatar.SizeMode = PictureBoxSizeMode.Zoom
         avatar.TabIndex = 0
         avatar.TabStop = False
@@ -55,9 +57,9 @@ Partial Class Form1
         ' PictureBox2
         ' 
         PictureBox2.Image = My.Resources.Resources.penny
-        PictureBox2.Location = New Point(384, 294)
+        PictureBox2.Location = New Point(432, 265)
         PictureBox2.Name = "PictureBox2"
-        PictureBox2.Size = New Size(47, 46)
+        PictureBox2.Size = New Size(53, 41)
         PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox2.TabIndex = 1
         PictureBox2.TabStop = False
@@ -65,9 +67,9 @@ Partial Class Form1
         ' enemy
         ' 
         enemy.Image = My.Resources.Resources.drake
-        enemy.Location = New Point(129, 52)
+        enemy.Location = New Point(145, 47)
         enemy.Name = "enemy"
-        enemy.Size = New Size(34, 43)
+        enemy.Size = New Size(38, 39)
         enemy.SizeMode = PictureBoxSizeMode.Zoom
         enemy.TabIndex = 2
         enemy.TabStop = False
@@ -75,9 +77,9 @@ Partial Class Form1
         ' enemy2
         ' 
         enemy2.Image = My.Resources.Resources.drake
-        enemy2.Location = New Point(796, 294)
+        enemy2.Location = New Point(896, 265)
         enemy2.Name = "enemy2"
-        enemy2.Size = New Size(37, 48)
+        enemy2.Size = New Size(42, 43)
         enemy2.SizeMode = PictureBoxSizeMode.Zoom
         enemy2.TabIndex = 3
         enemy2.TabStop = False
@@ -85,9 +87,9 @@ Partial Class Form1
         ' enemy3
         ' 
         enemy3.Image = My.Resources.Resources.drake
-        enemy3.Location = New Point(384, 160)
+        enemy3.Location = New Point(369, 114)
         enemy3.Name = "enemy3"
-        enemy3.Size = New Size(34, 41)
+        enemy3.Size = New Size(38, 37)
         enemy3.SizeMode = PictureBoxSizeMode.Zoom
         enemy3.TabIndex = 5
         enemy3.TabStop = False
@@ -96,23 +98,41 @@ Partial Class Form1
         ' platform
         ' 
         platform.BackColor = SystemColors.ActiveCaptionText
-        platform.Location = New Point(-1, 174)
+        platform.Location = New Point(-1, 157)
         platform.Name = "platform"
-        platform.Size = New Size(702, 17)
+        platform.Size = New Size(790, 15)
         platform.TabIndex = 6
         platform.TabStop = False
+        platform.Tag = "wall"
+        ' 
+        ' scorelabel
+        ' 
+        scorelabel.AutoSize = True
+        scorelabel.Font = New Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        scorelabel.Location = New Point(12, 9)
+        scorelabel.Name = "scorelabel"
+        scorelabel.Size = New Size(96, 34)
+        scorelabel.TabIndex = 7
+        scorelabel.Text = "Label1"
+        ' 
+        ' Timer2
+        ' 
+        Timer2.Enabled = True
+        Timer2.Interval = 1000
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(9F, 18F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(900, 404)
+        ClientSize = New Size(1012, 364)
+        Controls.Add(scorelabel)
         Controls.Add(platform)
         Controls.Add(enemy3)
         Controls.Add(enemy2)
         Controls.Add(enemy)
         Controls.Add(PictureBox2)
         Controls.Add(avatar)
+        Font = New Font("Microsoft Sans Serif", 9F)
         Name = "Form1"
         Text = "Form1"
         CType(avatar, ComponentModel.ISupportInitialize).EndInit()
@@ -122,6 +142,7 @@ Partial Class Form1
         CType(enemy3, ComponentModel.ISupportInitialize).EndInit()
         CType(platform, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents avatar As PictureBox
@@ -131,5 +152,7 @@ Partial Class Form1
     Friend WithEvents enemy2 As PictureBox
     Friend WithEvents enemy3 As PictureBox
     Friend WithEvents platform As PictureBox
+    Friend WithEvents scorelabel As Label
+    Friend WithEvents Timer2 As Timer
 
 End Class
--- tetris/tetris/Form1.vb
+++ tetris/tetris/Form1.vb
@@ -1,19 +1,35 @@
 ﻿Public Class Form1
+    Dim score As Integer
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         follow(enemy, avatar, 10, 0)
         follow(enemy2, avatar, 0, 5)
-        pace(enemy3, platform, 5)
+        PaceX(enemy3, platform, 5)
     End Sub
-    Sub pace(e As PictureBox, p As PictureBox, s As Integer)
+    Sub PaceX(e As PictureBox, p As PictureBox, speed As Integer)
         Dim dir As Integer
         dir = e.Tag
-        If e.Location.X > p.Location.X + p.Width Then
-            e.Location += New Point(dir * s, 0)
-        Else
-            e.Tag = -dir
-            e.Location += New Point(dir * s, 0)
-        End If
 
+        move(e, dir * speed, 0)
+
+        If e.Location.X > p.Location.X + p.Width / 2 Then
+            e.Tag = dir * -1
+        End If
+        If e.Location.X < p.Location.X Then
+            e.Tag = dir * -1
+        End If
+    End Sub
+    Sub PaceY(e As PictureBox, p As PictureBox, speed As Integer)
+        Dim dir As Integer
+        dir = e.Tag
+
+        move(e, 0, dir * speed)
+
+        If e.Location.Y < p.Location.Y Then
+            e.Tag = dir * -1
+        End If
+        If e.Location.Y > p.Location.Y + p.Height / 2 Then
+            e.Tag = dir * -1
+        End If
     End Sub
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
         If e.KeyCode = Keys.Left Or e.KeyCode = Keys.A Then
@@ -35,10 +51,23 @@
     End Sub
     Sub move(p As PictureBox, xdir As Integer, ydir As Integer)
         p.Location += New Point(xdir, ydir)
-        If p.Bounds.IntersectsWith(PictureBox2.Bounds) Then
-            PictureBox2.Visible = False
+        If IntersectsWith(p, "wall") Then
+            p.Location -= New Point(xdir, ydir)
         End If
     End Sub
+    Function IntersectsWith(p As PictureBox, tag As String) As Boolean
+        For Each o In Controls
+            Dim obj As PictureBox
+            obj = TryCast(o, PictureBox)
+            If Not obj Is Nothing Then
+                If p.Bounds.IntersectsWith(obj.Bounds) And UCase(obj.Tag) = UCase(tag) Then
+                    Return True
+                End If
+            End If
+        Next
+        Return False
+    End Function
+
     Sub follow(e As PictureBox, a As PictureBox, speedx As Integer, speedy As Integer)
         If e.Location.Y < a.Location.Y Then
             move(e, 0, speedy)
@@ -50,5 +79,8 @@
         End If
     End Sub
 
-
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        score += 1
+        scorelabel.Text = score
+    End Sub
 End Class
